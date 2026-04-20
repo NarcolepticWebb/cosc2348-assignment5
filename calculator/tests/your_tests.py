49c772a1c758b8b0bafe940dd5fed397fdb0b8a2
Hayden <hwebb2@islander.tamucc.edu>
2026-04-20 03:52:28 +0000

    added Makefile and CI tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-from calculator_adapter import run
+import subprocess
 
+def test_addition():
+    result = subprocess.run(["./calculator", "2", "+", "3"], capture_output=True, text=True)
+    assert "5" in result.stdout
 
-### ADD AT LEAST TWO TESTS HERE!
-
-
-###
-
-print("All tests passed!")
+def test_multiplication():
+    result = subprocess.run(["./calculator", "4", "*", "5"], capture_output=True, text=True)
+    assert "20" in result.stdout
